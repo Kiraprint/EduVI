b6c2905a761a39d0b17cb1a54fc4fce05d30e779
Kiraprint <76822921+Kiraprint@users.noreply.github.com>
2021-04-26 22:40:41 +0300

    Added htmls

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_restful import reqparse, abort, Api, Resource
 import datetime
-
+'''
 import homeworks_resources
-from data import db_session
-
-app = Flask(__name__)
 app.config['SECRET_KEY'] = 'R27yufghz1321'
 app.config['PERMANENT_SESSION_LIFETIME'] = datetime.timedelta(days=365)
 api = Api(app)
@@ -13,10 +10,20 @@ api = Api(app)
 api.add_resource(homeworks_resources.HomeworksListResources, '/api/v2/homeworks')
 # для одного объекта
 api.add_resource(homeworks_resources.HomeworkResources, '/api/v2/news/<int:homeworks_id>')
+'''
+from data import db_session
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'R27yufghz1321'
 
 def main():
-    db_session.global_init("db/homeworks.db")
+    # db_session.global_init("db/homeworks.db")
     app.run()
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
