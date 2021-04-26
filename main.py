3b930a12c5b0615773477b35dcb9ced3f533f1f3
Kiraprint <76822921+Kiraprint@users.noreply.github.com>
2021-04-27 00:57:48 +0300

    fixed all htmls

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from flask_restful import reqparse, abort, Api, Resource
 import datetime
+
 '''
 import homeworks_resources
 app.config['SECRET_KEY'] = 'R27yufghz1321'
@@ -16,14 +17,21 @@ from data import db_session
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'R27yufghz1321'
 
+
 def main():
     # db_session.global_init("db/homeworks.db")
     app.run()
 
+
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('/index.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('/login.html')
 
 
 if __name__ == '__main__':
---
+++ static/js/zooming.js
@@ -0,0 +1,2 @@
+var img = document.getElementsByClassName('img-wrap');
+img.addEventListener("mouseover", function(event){img.classList.toggle('active_zoom')})
