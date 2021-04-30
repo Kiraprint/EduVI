4cfeb55cb682e4e5ad97a10bd8e1785db5c673b1
Kiraprint <76822921+Kiraprint@users.noreply.github.com>
2021-04-30 21:39:39 +0300

    mb editted the login error

--- data/db_session.py
+++ data/db_session.py
@@ -1,11 +1,13 @@
 import sqlalchemy as sa
+import sqlalchemy.ext.declarative as dec
 import sqlalchemy.orm as orm
 from sqlalchemy.orm import Session
-import sqlalchemy.ext.declarative as dec
 
 SqlAlchemyBase = dec.declarative_base()
 
 __factory = None
+
+
 def global_init(db_file):
     global __factory
 
@@ -21,12 +23,11 @@ def global_init(db_file):
     engine = sa.create_engine(conn_str, echo=False)
     __factory = orm.sessionmaker(bind=engine)
 
-    from . import __all_models
-
     SqlAlchemyBase.metadata.create_all(engine)
 
 
 def create_session() -> Session:
     global __factory
+    if not __factory:
+        global_init("db/homeworks.db")
     return __factory()
-
